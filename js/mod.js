ff7f321f35a4f54111de151a08ac68a0f5bd269f
mind1679 <70496566+mind1679@users.noreply.github.com>
2022-01-18 12:36:31 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -43,7 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if(hasUpgrade("p", 11)) gain = gain.mul(2)
+	if(hasUpgrade("Pr", 11)) gain = gain.mul(2)
 	return gain
 }
 
